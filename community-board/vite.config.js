efd13f26912640a43d43a6511943d014f963b766
Ladayport <z0955600718@gmail.com>
2026-02-02 18:00:27 +0800

    feat: 新增 Vite 設定檔以支援 React 並設定部署基礎路徑

--- community-board/vite.config.js
+++ community-board/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/bulletin_board_googlesheet/',
+    base: '/bulletin_board_googleSheet/',
 })
